dd8d02302fdf8d258b0c8a38b55f0187d2d6dff3
BWbwchen <tim.chenbw@gmail.com>
2023-04-24 15:33:10 +0800

    [feat   ]: add a lot of apps in hosts/apps.nix

--- hosts/apps.nix
+++ hosts/apps.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
-{
+with pkgs;
+let default-python = python3.withPackages ( python-packages:
+        with python-packages; [
+            pip
+        ]);
+in {
+  nixpkgs.config.allowUnfree = true;
   imports = [
     ../modules/git.nix
     ../modules/zsh.nix
@@ -8,21 +14,32 @@
     ../modules/emacs
   ];
   home.packages = with pkgs; [
+    # GUI utilities
+    arandr # visual front end for xrandr
+      
     # Command line utilities
     neovim
     wget
     curl
     htop
     neofetch
+    zip
     unzip
+    unrar
     ripgrep
     file
+    appimage-run
+    any-nix-shell # zsh support for nix-shell
+    tree
+    gnupg
+    feh
     
 
     # Programming languages
     gcc
-    python3
+    default-python
     nodejs
+    nixfmt
     cargo
     rustc
     gnumake

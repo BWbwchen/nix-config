5eafaf7bfbe649db73536406a4356041065d2bb8
BWbwchen <tim.chenbw@gmail.com>
2023-06-23 12:27:35 +0800

    [feat   ]: use qwbarch's configuration.nix
    ref: https://github.com/qwbarch/nix-config

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -4,35 +4,16 @@
   imports = [
     # include the hardware setting
     ./hardware-configuration.nix
-    ../../modules/i3
   ];
 
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    xkbVariant = "";
-    xkbOptions = "ctrl:nocaps";
-  };
-
-  services.blueman.enable = true;
-
-  services.openssh.enable = true;
-  services.qemuGuest.enable = true;
-
   boot.loader = {
+    # systemd-boot.enable = true;
     grub = {
       enable = true;
       device = "/dev/sda";
       useOSProber = true;
     };
   };
-
-  # Automatically delete generations older than a week
-  nix.gc = {
-    automatic = true;
-    options = "--delete-generations 8d";
-  };
-
   networking = {
     hostName = "${user}-vm";
     networkmanager.enable = true;
@@ -42,6 +23,7 @@
 
   time.timeZone = "Asia/Taipei";
 
+  # Select internationalisation properties.
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
@@ -57,20 +39,118 @@
     };
   };
 
+  services = {
+    gvfs.enable = true; # Nautilus file manager.
+    joycond.enable = true;
+    blueman.enable = true;
+    openssh.enable = true;
+    qemuGuest.enable = true;
+    dbus = {
+      enable = true;
+      packages = [ pkgs.dconf ];
+    };
+
+    xserver = {
+      enable = true;
+      layout = "us";
+      xkbVariant = "";
+      xkbOptions = "ctrl:nocaps";
+      # videoDrivers = [ "nvidia" ];
+      libinput = {
+        enable = true;
+        mouse = {
+          accelProfile = "flat"; # Disable acceleration.
+          middleEmulation =
+            false; # Disable emulating middle click using left + right clicks;
+        };
+      };
+
+      displayManager = {
+        defaultSession = "none+i3";
+        sddm.autoNumlock = true;
+
+        # autoLogin = {
+        #   enable = true;
+        #   user = username;
+        # };
+
+        # Enable 240hz refresh rate.
+        setupCommands =
+          "${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --mode 1920x1080 --rate 239.76";
+      };
+
+      windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+      };
+    };
+  };
+
+  console.useXkbConfig = true;
+
+  hardware = {
+    pulseaudio.enable = true;
+    bluetooth.enable = true;
+    ledger.enable = true; # Allow ledger devices to connect.
+    opengl.driSupport32Bit = true; # Required for steam.
+  };
+
+  # Enable docker in rootless mode.
+  # virtualisation.docker.rootless = {
+  #   enable = true;
+  #   setSocketVariable = true;
+  # };
+
   users.users.${user} = {
     isNormalUser = true;
     description = "${user}";
-    extraGroups = [ "wheel" "networkmanager" ];
-    initialPassword = "password";
+    extraGroups = [ "networkmanager" "wheel" "audio" "docker" ];
+    packages = with pkgs; [ ];
     shell = pkgs.zsh;
   };
 
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  environment = {
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    systemPackages = with pkgs; [
+      git
+      # docker-compose
+      gnome.adwaita-icon-theme
+      gnome.nautilus
+    ];
+
+    # Disable gui prompt when git asks for a password.
+    extraInit = ''
+      unset -v SSH_ASKPASS
+    '';
+  };
+
+  fonts.fonts = with pkgs; [ nerdfonts ];
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ 3000 8080 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+
   nix = {
+    # package = pkgs.nixFlakes;
     package = pkgs.nixVersions.stable; # flakes
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
     extraOptions = "experimental-features = nix-command flakes";
   };
+
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # services.xrdp.enable = true;
+  # services.xrdp.defaultWindowManager = "i3";
+  # services.xrdp.openFirewall = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions

41bb84ffa39e22f772fc1a8cb5b5b2023e42a99f
BWbwchen <tim.chenbw@gmail.com>
2023-06-23 23:44:02 +0800

    [feat   ]: firefox setting

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -7,6 +7,16 @@
       default = {
         isDefault = true;
         settings = {
+          "browser.display.background_color" = "#FFFFFF";
+          "browser.display.background_color.dark" = "#1C1B22";
+          "browser.display.foreground_color" = "#000000";
+          "browser.display.foreground_color.dark" = "#FBFBFE";
+          "browser.search.hiddenOneOffs" =
+            "Google,Amazon.com,Bing,DuckDuckGo,Wikipedia (en)";
+          "browser.search.suggest.enabled" = false;
+          "browser.urlbar.placeholderName" = "Google";
+          "browser.urlbar.speculativeConnect.enabled" = true;
+          "general.smoothScroll" = true;
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         };
 

5741e04c2993de57619a2327dbeea64e1fda9512
BWbwchen <tim.chenbw@gmail.com>
2023-06-25 23:16:12 +0800

    [feat   ]: add audio
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -107,8 +107,19 @@
   console.useXkbConfig = true;
 
   sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
   hardware = {
-    pulseaudio.enable = true;
+    pulseaudio = {
+      enable = false; # Since we use pipewire
+      # extraConfig = "load-module module-combine-sink";
+    };
     bluetooth.enable = true;
     # ledger.enable = true; # Allow ledger devices to connect.
     # opengl = {};

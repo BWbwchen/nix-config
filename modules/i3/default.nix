aa945a8673b1561036dc81a19ccbc84c874e40a2
BWbwchen <tim.chenbw@gmail.com>
2023-06-26 10:09:06 +0800

    [style  ]: i3 match the old config
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

--- modules/i3/default.nix
+++ modules/i3/default.nix
@@ -16,10 +16,20 @@ in {
         window = {
           border = 0;
           hideEdgeBorders = "both";
-          commands = [{
-            command = "border pixel 5";
-            criteria = { class = "^.*"; };
-          }];
+          commands = [
+            {
+              command = "border pixel 5";
+              criteria = { class = "^.*"; };
+            }
+            {
+              command = "move to workspace $ws1";
+              criteria = { class = "Google"; };
+            }
+            {
+              command = "move to workspace $ws1";
+              criteria = { class = "Firefox"; };
+            }
+          ];
         };
 
         gaps = {
@@ -77,25 +87,25 @@ in {
           "${modifier}+f" = "exec ${pkgs.gnome.nautilus}/bin/nautilus";
 
           # WorkSpace
-          "${modifier}+1" = "workspace 1";
-          "${modifier}+2" = "workspace 2";
-          "${modifier}+3" = "workspace 3";
-          "${modifier}+4" = "workspace 4";
-          "${modifier}+5" = "workspace 5";
-          "${modifier}+6" = "workspace 6";
-          "${modifier}+7" = "workspace 7";
-          "${modifier}+8" = "workspace 8";
-          "${modifier}+9" = "workspace 9";
+          "${modifier}+1" = "workspace $ws1";
+          "${modifier}+2" = "workspace $ws2";
+          "${modifier}+3" = "workspace $ws3";
+          "${modifier}+4" = "workspace $ws4";
+          "${modifier}+5" = "workspace $ws5";
+          "${modifier}+6" = "workspace $ws6";
+          "${modifier}+7" = "workspace $ws7";
+          "${modifier}+8" = "workspace $ws8";
+          "${modifier}+9" = "workspace $ws9";
 
-          "${modifier}+Shift+1" = "move container to workspace 1";
-          "${modifier}+Shift+2" = "move container to workspace 2";
-          "${modifier}+Shift+3" = "move container to workspace 3";
-          "${modifier}+Shift+4" = "move container to workspace 4";
-          "${modifier}+Shift+5" = "move container to workspace 5";
-          "${modifier}+Shift+6" = "move container to workspace 6";
-          "${modifier}+Shift+7" = "move container to workspace 7";
-          "${modifier}+Shift+8" = "move container to workspace 8";
-          "${modifier}+Shift+9" = "move container to workspace 9";
+          "${modifier}+Shift+1" = "move container to workspace $ws1";
+          "${modifier}+Shift+2" = "move container to workspace $ws2";
+          "${modifier}+Shift+3" = "move container to workspace $ws3";
+          "${modifier}+Shift+4" = "move container to workspace $ws4";
+          "${modifier}+Shift+5" = "move container to workspace $ws5";
+          "${modifier}+Shift+6" = "move container to workspace $ws6";
+          "${modifier}+Shift+7" = "move container to workspace $ws7";
+          "${modifier}+Shift+8" = "move container to workspace $ws8";
+          "${modifier}+Shift+9" = "move container to workspace $ws9";
 
           # Audio Volume
           "XF86AudioRaiseVolume" =
@@ -143,9 +153,9 @@ in {
         ];
       };
       extraConfig = ''
-        set $ws1 1
-        set $ws2 2
-        set $ws3 3
+        set $ws1 "1:www"
+        set $ws2 "2:emacs"
+        set $ws3 "3:term"
         set $ws4 4
         set $ws5 5
         set $ws6 6
